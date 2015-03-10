2b8ff9b51b17d0d888848cf1e5838ba4a7bb70bc
Shane Bonner <sbonner1@ycp.edu>
2015-03-10 16:49:47 -0400

    fixed the Servlet class so the database will work, and added a jettyRunner

--- app/src/main/java/cs/ycp/edu/cs481/ratemydrink/URLInfo.java
+++ app/src/main/java/cs/ycp/edu/cs481/ratemydrink/URLInfo.java
@@ -4,5 +4,5 @@ package cs.ycp.edu.cs481.ratemydrink;
  * A simple class to store the url of the server
  */
 public class URLInfo {
-    public static final String DOMAIN_URL = "http://localhost:8080";
+    public static final String DOMAIN_URL = "http://10.0.2.2:8080/backend/";
 }
