f063cb8958c05ee71d5d6e75d3fe53aeb65ea528
Shane Bonner <sbonner1@ycp.edu>
2015-03-30 13:36:21 -0400

    added comment class. not finished

---
+++ modelclasses/src/main/java/com/rateMyDrink/modelClasses/Comment.java
@@ -0,0 +1,12 @@
+package com.rateMyDrink.modelClasses;
+
+/**
+ * Created by shanembonner on 3/30/15.
+ */
+public class Comment {
+    private int drinkId;
+    private String username;
+    private String comment;
+
+
+}
