6761d50b742593faefb57de1e57fcc7aa39d9bfb
Josh Coady <droidmaker42@gmail.com>
2015-04-09 19:51:32 -0400

    refactored IGetBeer & IPostNewBeer into IBeerRequests

--- app/src/main/java/cs/ycp/edu/cs481/ratemydrink/controllers/web_controllers/ILiquorRequests.java
+++ app/src/main/java/cs/ycp/edu/cs481/ratemydrink/controllers/web_controllers/ILiquorRequests.java
@@ -14,9 +14,22 @@ import retrofit.http.Query;
  */
 public interface ILiquorRequests {
 
+    /**
+     * retrives a Liquor object from the database with the given id
+     *
+     * @param action the "action" parameter in the URL (should always be 'getLiquor'
+     * @param id the "id" parameter in the URL, should be the id of the Liquor object to be retrived from the database
+     * @return the Liquor object with the given id
+     */
     @GET("/")
     Liquor get(@Query("action") String action ,@Query("id") int id);
 
+    /**
+     * POSTs a Liquor object to the database
+     *
+     * @param newLIQUOR the Liquor object to be added to the datatbase
+     * @return the Liquor object that was just added to the database
+     */
     @Headers({"Content-Type: application/json"})
     @POST("/backend/?action=addLiquor")
     Liquor post(@Body Liquor newLIQUOR);
